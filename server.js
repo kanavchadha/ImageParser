71c8e8fb367e28edcef1ebe41dc66b6e021b4bf6
kanavchadha <kanavchadha56@gmail.com>
2024-01-01 20:31:57 +0530

    app ready - v1

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
     //allowed extensions
-    const filetypes = /img|jpg|jpeg|png|webp/;
+    const filetypes = /img|jpg|jpeg|png|webp|gif/;
 
     //check extensions
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
@@ -32,27 +32,24 @@ const upload = multer({ storage: storage, limits: { fileSize: 2000000 }, fileFil
 
 app.use(express.static(__dirname + '/public'));
 
-const EXPENSE_CATEGORIES = ['Category', 'Education', 'Food/Eatables', 'Bills', 'Rent', 'Home Needs', 'Vehicles', 'Child', 'Medical', 'Beauty and Care', 'Clothing', 'Enjoyment', 'Personal Need', 'Investment', 'Gifts', 'Other', 'Monthly Income'];
-const INVESTMENT_CATEGORIES = ['Category', 'Fixed Deposit', 'PPF', 'Stocks', 'KVP', 'Mutual Funds', 'NSC', 'SCSS', 'Monthly Income Scheme', 'Life Insurance', 'Other'];
-
 app.get('/', async (req, res) => {
     return res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/parse-expenso-file', upload.array('file', 10), async (req, res) => {
+app.post('/parse-expenso-file', upload.array('file', 20), async (req, res) => {
     let result = [];
     try {
         for (file of req.files) {
             const worker = await createWorker('eng');
             const ret = await worker.recognize(file.path);
             await worker.terminate();
-            const data = { text: ret.data.text, lines: ret.data.lines.map(line => line.text) };
-            if (!data.lines) data.length = [];
+            const data = { text: ret.data.text, lines: ret.data.lines?.map(line => line.text) };
+            if(process.env.printData) console.log(data.lines);
+            if (!data.lines) data.lines = [];
             for (let i = data.lines.length - 1; i >= 0; i--) {
                 try {
                     if (!data.lines[i]) continue;
                     const line = data.lines[i].replace('\\n', '');
-                    console.log(line);
                     if (line.search(/^Received|Paid|Sent|Added/gi) === 0) {
                         let title = '', amount = 0, pline = data.lines[i - 1], j = i - 1;
                         while (j >= 0 && pline.search(/^Received|Paid|Sent|Added/gi) !== 0) {
@@ -60,26 +57,20 @@ app.post('/parse-expenso-file', upload.array('file', 10), async (req, res) => {
                             let titleOrAmt = pline.split(/\s\-\S/g);
                             if (titleOrAmt.length > 1) {
                                 title = titleOrAmt[0]
-                                amount = Number(titleOrAmt[1].split(',').join(''));
+                                amount = Number(titleOrAmt[1].replaceAll(/\D/gi, ''));
                                 if (!isNaN(amount)) break;
                             }
                             titleOrAmt = pline.split(/\s\+\S/g);
                             if (titleOrAmt.length > 1) {
                                 title = titleOrAmt[0]
-                                amount = Number(titleOrAmt[1].split(',').join(''));
+                                amount = Number(titleOrAmt[1].replaceAll(/\D/gi, ''));
                                 if (!isNaN(amount)) break;
                             }
                             j--;
                             pline = data.lines[j];
                         }
-                        const dateParts = line.split(', ');
-                        let timeStamp = new Date();
-                        if (dateParts.length > 1) {
-                            let date = dateParts[0].split(' '), time = dateParts[1].slice(0, 8);
-                            date = isNaN(date.at(-1)) ? `${date.at(-2)} ${date.at(-1)} ${timeStamp.getFullYear()}` : `${date.at(-3)} ${date.at(-2)} ${date.at(-1)}`;
-                            timeStamp = new Date(date + ' ' + time);
-                        }
-                        result.push({ title, amount, date: timeStamp.toISOString() })
+                        const date = parseDate(line);
+                        result.push({ title, amount, date, category: getRandomCategory(), description: title, status: 'true' })
                         i = j + 1;
                     }
                 } catch (error) {
@@ -93,6 +84,33 @@ app.post('/parse-expenso-file', upload.array('file', 10), async (req, res) => {
         return res.status(500).send({ result, error: `Something Went Wrong! ${err.message}` });
     }
 })
+
+const parseDate = (line) => {
+    let timeStamp = new Date();
+    const dateParts = line.split(', ');
+    if (dateParts.length > 1) {
+        let date = dateParts[0].split(' '), time = dateParts[1].slice(0, 8);
+        if (date.length === 2 && isNaN(date.at(-1)) && isNaN(date.at(-2))) {
+            if (date.at(-1) === 'Yesterday'){
+                date = (new Date(timeStamp.getFullYear(), timeStamp.getMonth(), timeStamp.getDate()-1)).toDateString();
+            } else {
+                date = timeStamp.toDateString()
+            }
+        } else if (isNaN(date.at(-1)) && !isNaN(date.at(-2))) {
+            date = `${date.at(-2)} ${date.at(-1)} ${timeStamp.getFullYear()}`
+        } else {
+            date = `${date.at(-3)} ${date.at(-2)} ${date.at(-1)}`
+        }
+        timeStamp = new Date(date + ' ' + time);
+    }
+    return timeStamp.toISOString();
+}
+
+const getRandomCategory = () => {
+    const EXPENSE_CATEGORIES = ['Education', 'Food/Eatables', 'Bills', 'Rent', 'Home Needs', 'Vehicles', 'Child', 'Medical', 'Beauty and Care', 'Clothing', 'Enjoyment', 'Personal Need', 'Investment', 'Gifts', 'Other', 'Monthly Income'];
+    const randomInd = Math.floor(Math.random() * EXPENSE_CATEGORIES.length);
+    return EXPENSE_CATEGORIES[randomInd];
+}
 
 app.use('*', (req, res) => {
     return res.redirect('/');
